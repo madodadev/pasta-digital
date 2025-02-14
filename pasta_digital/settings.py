4879107057e7257559be9c45183bad93d933b145
Assedio Horacio <assedioassedio7@gmail.com>
2025-02-14 12:13:53 +0200

    setting language and time

--- pasta_digital/settings.py
+++ pasta_digital/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-MZ'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Maputo'
 
 USE_I18N = True
 
