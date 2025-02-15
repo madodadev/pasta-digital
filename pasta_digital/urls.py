dc9be7c7d577aecbe426f9d3a1315c81bff0ee44
Assedio Horacio <assedioassedio7@gmail.com>
2025-02-15 11:34:12 +0200

    add resultados_estudantes app

--- pasta_digital/urls.py
+++ pasta_digital/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('resultados/', include('resultados_estudantes.urls'))
 ]
